5e54df7dbc2e914d09ae4e81fa848c879763c9ba
HelittonKieras <108702108+HelittonKieras@users.noreply.github.com>
2022-07-10 20:44:48 -0300

    Add files via upload
    [M1S02] Ex 3 - Javascript

---
+++ [M1S02] Ex 3 - Javascript.js
@@ -0,0 +1,5 @@
+var a = prompt("Digite um numero inteiro: ");
+var b = prompt("Digite outro numero inteiro: ");
+var soma;
+soma= parseInt(a)+parseInt(b)
+alert("A soma dos numeros digitados é: ", + soma);
