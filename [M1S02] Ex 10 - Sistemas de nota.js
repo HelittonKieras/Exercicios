09ff691d64c6c8d975fc46915325c6093c993764
HelittonKieras <108702108+HelittonKieras@users.noreply.github.com>
2022-07-11 19:42:28 -0300

    Add files via upload
    [M1S02] Ex 10 - Sistemas de nota

---
+++ [M1S02] Ex 10 - Sistemas de nota.js
@@ -0,0 +1,13 @@
+var nome = prompt("Informe seu nome");
+
+
+var n1 = prompt("Digite a 1º Nota");
+var n2 = prompt("Digite a 2º Nota");
+var n3 = prompt("Digite a 3º Nota");
+var n4 = prompt("Digite a 4º Nota");
+
+
+var media = (Number(n1)+Number(n2)+Number(n3)+Number(n4))/4
+
+
+alert('A média do aluno '+nome+'  foi: '+media)
